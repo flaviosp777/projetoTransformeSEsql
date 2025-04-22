1f6b587a39ada2632bed3a389c7da05934e01787
flaviosp777 <flavio.jcjesus@senacsp.edu.br>
2025-04-22 16:47:57 -0300

    atualização

---
+++ ProjetoDataGridView/frmDataGridView.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace ProjetoDataGridView
+{
+    partial class frmDataGridView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvDados = new System.Windows.Forms.DataGridView();
+            this.codFunc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TelCel = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cpf = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.status = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.perfil = new System.Windows.Forms.DataGridViewImageColumn();
+            this.pagina = new System.Windows.Forms.DataGridViewLinkColumn();
+            this.selecione = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDados
+            // 
+            this.dgvDados.AllowUserToAddRows = false;
+            this.dgvDados.AllowUserToDeleteRows = false;
+            this.dgvDados.AllowUserToOrderColumns = true;
+            this.dgvDados.AllowUserToResizeColumns = false;
+            this.dgvDados.AllowUserToResizeRows = false;
+            this.dgvDados.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDados.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.codFunc,
+            this.nome,
+            this.email,
+            this.TelCel,
+            this.cpf,
+            this.status,
+            this.perfil,
+            this.pagina,
+            this.selecione});
+            this.dgvDados.Location = new System.Drawing.Point(12, 225);
+            this.dgvDados.Name = "dgvDados";
+            this.dgvDados.Size = new System.Drawing.Size(665, 173);
+            this.dgvDados.TabIndex = 0;
+            // 
+            // codFunc
+            // 
+            this.codFunc.HeaderText = "Código";
+            this.codFunc.Name = "codFunc";
+            // 
+            // nome
+            // 
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.Padding = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.nome.DefaultCellStyle = dataGridViewCellStyle2;
+            this.nome.HeaderText = "Nome";
+            this.nome.Name = "nome";
+            // 
+            // email
+            // 
+            this.email.HeaderText = "E-mail";
+            this.email.Name = "email";
+            // 
+            // TelCel
+            // 
+            this.TelCel.HeaderText = "Telefone";
+            this.TelCel.Name = "TelCel";
+            // 
+            // cpf
+            // 
+            this.cpf.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.cpf.HeaderText = "CPF";
+            this.cpf.Name = "cpf";
+            // 
+            // status
+            // 
+            this.status.HeaderText = "Status";
+            this.status.Name = "status";
+            // 
+            // perfil
+            // 
+            this.perfil.HeaderText = "Perfil";
+            this.perfil.Name = "perfil";
+            // 
+            // pagina
+            // 
+            this.pagina.HeaderText = "Página";
+            this.pagina.Name = "pagina";
+            // 
+            // selecione
+            // 
+            this.selecione.HeaderText = "Selecione";
+            this.selecione.Name = "selecione";
+            // 
+            // frmDataGridView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 410);
+            this.Controls.Add(this.dgvDados);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MinimizeBox = false;
+            this.Name = "frmDataGridView";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Data Grid View";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDados;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codFunc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TelCel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cpf;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn status;
+        private System.Windows.Forms.DataGridViewImageColumn perfil;
+        private System.Windows.Forms.DataGridViewLinkColumn pagina;
+        private System.Windows.Forms.DataGridViewComboBoxColumn selecione;
+    }
+}
